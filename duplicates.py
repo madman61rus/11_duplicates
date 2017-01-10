78a410843843d814cf3b3b2024da0ae9767fab49
madman61rus <rhjnhjtn3yjhe>
2017-01-11 00:25:11 +0300

    - Исправил нарушения PEP8 -+ заменил domain_name.split('//')[1] на встроенную функцию urlparse -+ заменил многословный лишний кусок [line for line in file.read().splitlines()] на простое чтение из файла сразу в список - Убрал вывод в консоль в функции load_urls4check. Она и так возвращает None в случае неудаче. При выводе есть проверка

--- duplicates.py
+++ duplicates.py
@@ -1,8 +1,22 @@
-
+import argparse
+import os
 
 def are_files_duplicates(file_path1, file_path_2):
     pass
 
+def get_all_path_info(path):
+    files = []
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            files.append({
+                'full_path' : os.path.join(root,file) ,
+            'file_name' : file})
+            print(files)
 
 if __name__ == '__main__':
-    pass
+    args = argparse.ArgumentParser()
+    args.add_argument('-p','--path',help='A Path for searching duplicates',required=True)
+    arguments = args.parse_args()
+
+    if arguments.path :
+        get_all_path_info(arguments.path)
