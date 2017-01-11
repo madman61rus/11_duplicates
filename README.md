d72de3d35032067e07e32a17b7c613bdda223e4f
Vladimir Kompaniec <vkompaniec@gmail.com>
2017-01-11 12:24:09 +0300

    + Закончил написание программы для поиска дубликатов

--- README.md
+++ README.md
@@ -1,7 +1,30 @@
 # Anti-Duplicator
 
-[TODO. There will be project description]
+Данная программа ищет в указанной папке дубликаты файлов и если они
+найдены, выводит их на экран. Критерием дублирования является название
+файла и его размер
 
-# Project Goals
+### Как использовать
 
-The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
+Для запуска программы, ей необходимо указать путь, по которому
+нужно искать дубликаты, с помощью ключа -p или --path
+
+Пример использования программы
+```
+python dublicates.py -p ~/test_path
+```
+
+Пример вывода на консоль
+```
+Найден дубликат файла python-learn.txt, путь /home/vkompaniec/PycharmProjects/11_duplicates/test/3/33/python-learn.txt, размер 7848
+Найден дубликат файла python-learn.txt, путь /home/vkompaniec/PycharmProjects/11_duplicates/test/2/python-learn.txt, размер 7848
+Найден дубликат файла statement (5).pdf, путь /home/vkompaniec/PycharmProjects/11_duplicates/test/1/statement (5).pdf, размер 112624
+Найден дубликат файла statement (5).pdf, путь /home/vkompaniec/PycharmProjects/11_duplicates/test/1/11/statement (5).pdf, размер 112624
+Найден дубликат файла statement (5).pdf, путь /home/vkompaniec/PycharmProjects/11_duplicates/test/statement (5).pdf, размер 112624
+Найден дубликат файла Алексей Юхнин_Презентация.pdf, путь /home/vkompaniec/PycharmProjects/11_duplicates/test/1/11/Алексей Юхнин_Презентация.pdf, размер 472681
+Найден дубликат файла Алексей Юхнин_Презентация.pdf, путь /home/vkompaniec/PycharmProjects/11_duplicates/test/1/Алексей Юхнин_Презентация.pdf, размер 472681
+```
+
+# Цели проекта
+
+Этот код написан в целях обучения. Курс для веб-разработчиков - [DEVMAN.org](https://devman.org)
--- duplicates.py
+++ duplicates.py
@@ -1,22 +1,49 @@
 import argparse
 import os
 
-def are_files_duplicates(file_path1, file_path_2):
-    pass
+
+def are_files_duplicates(file_1, file_2):
+    if not (file_1['full_path'] == file_2['full_path']) and \
+            (file_1['file_name'] == file_2['file_name']) and \
+            (file_1['file_size'] == file_2['file_size']):
+        return True
+    else:
+        return False
+
 
 def get_all_path_info(path):
-    files = []
+    files_info = []
     for root, dirs, files in os.walk(path):
         for file in files:
-            files.append({
-                'full_path' : os.path.join(root,file) ,
-            'file_name' : file})
-            print(files)
+            files_info.append({
+                'full_path': os.path.join(root, file),
+                'file_name': file,
+                'file_size': os.path.getsize(os.path.join(root, file))})
+
+    return files_info
+
+
+def print_all_duplicates(duplicates):
+    for dublicate in sorted(duplicates, key=lambda dbc: dbc['file_name']):
+        message = "Найден дубликат файла {}, путь {}, размер {}"
+        print(message.format(dublicate['file_name'], dublicate['full_path'], dublicate['file_size']))
+
+
+def get_all_dublicates(files_info):
+    dublicates = []
+    for file_1 in files_info:
+        for file_2 in files_info:
+            if are_files_duplicates(file_1=file_1, file_2=file_2) \
+                    and file_2 not in dublicates:
+                dublicates.append(file_2)
+    return dublicates
+
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
-    args.add_argument('-p','--path',help='A Path for searching duplicates',required=True)
+    args.add_argument('-p', '--path', help='Путь к папке для поиска', required=True)
     arguments = args.parse_args()
 
-    if arguments.path :
-        get_all_path_info(arguments.path)
+    if arguments.path:
+        dublicated_files = get_all_dublicates(get_all_path_info(arguments.path))
+        print_all_duplicates(dublicated_files)
